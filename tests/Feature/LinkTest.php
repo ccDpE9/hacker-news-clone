fc37f380572f459de807aeb651d201de416203b6
Kosta__ <z_eon00@protonmail.com>
2018-10-20 17:58:31 +0200

    User can see link's comments test

--- tests/Feature/LinkTest.php
+++ tests/Feature/LinkTest.php
@@ -12,59 +12,79 @@ class LinkTest extends TestCase
 
     use DatabaseMigrations;
 
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->link = factory('App\Link')->create();
+    }
+
+
     /** @test **/
     public function user_can_view_index_page()
     {
-        $response = $this->get('/links');
-        $response->assertStatus(200);
+        $this->get('/links')
+            ->assertStatus(200);
     }
+
 
     /** @test **/
     public function user_can_view_links()
     {
-        $link = factory('App\Link')->create();
-        $response = $this->get('/links');
-        $response->assertSee($link->title);
+        $this->get('/links')
+            ->assertSee($this->link->title);
     }
+
 
     /** @test **/
     public function user_can_view_individual_link()
     {
-        $link = factory('App\Link')->create();
-        $response = $this->get('/links/' . $link->id);
-        $response->assertSee($link->title);
+        $this->get('/links/' . $this->link->id)
+            ->assertSee($this->link->title);
     }
+
 
     /** @test **/
     public function auth_user_can_post_link()
     {
         $user = factory(\App\User::class)->create();
-        $response = $this->actingAs($user);
+        $this->actingAs($user);
         // $link = factory('App\Link')->make();
         // code above drops an error, why?
-        $link = factory('App\Link')->create();
-        $this->post('/links/store', $link->toArray());
-        $test = $this->get('/links/' . $link->id);
-        $test->assertSee($link->title);
+        $this->post('/links/store', $this->link->toArray());
+        $this->get('/links/' . $this->link->id)
+            ->assertSee($this->link->title);
     }
+
 
     /** @test **/
     public function non_authenticated_users_cannot_post_link()
     {
-        $link = factory('App\Link')->create();
-        $request = $this->post('/links/store', $link->toArray());
-        $request->assertStatus(405);
+        $this->post('/links/store', $this->link->toArray())
+            ->assertStatus(405);
     }
+
 
     /** @test **/
     public function a_link_can_be_deleted()
     {
         $user = factory('App\User')->create();
         $this->actingAs($user);
-        $link = factory('App\Link')->create();
         //$this->json('DELETE', $link->path());
-        $this->delete('/links/' . $link->id);
-        $this->assertDatabaseMissing('links', ['id' => $link->id]);
+        $this->delete('/links/' . $this->link->id);
+        $this->assertDatabaseMissing('links', ['id' => $this->link->id]);
     }
 
+
+    /** @test **/
+    public function a_user_can_see_links_comments()
+    {
+        $comment = factory('App\Comment')
+            ->create([
+                'commentable_id' => $this->link->id,
+                'commentable_type' => 'App\Link',
+            ]);
+        $this->get('/links/' . $this->link->id)
+            ->assertSee($comment->body);
+    }
 }
