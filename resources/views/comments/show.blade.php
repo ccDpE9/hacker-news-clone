de985c3350f242a672b45bbc965918193bf055fb
Kosta__ <z_eon00@protonmail.com>
2018-10-26 18:32:59 +0200

    Basic comment styling template

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,7 +1,14 @@
 @foreach ($comments as $comment)
 
-    <div class="reply">
-        <p>{{ $comment->body }}</p>
+    <div class="thread__reply comment">
+        <span class="thread__reply--author">
+            <a href="#">{{ $comment->user->name }}</a>
+        </span>
+        <span class="thread__reply--date">
+            {{ $comment->created_at }}
+        </span>
+        <p class="thread__reply--body">{{ $comment->body }}</p>
+        <p class="btn--reply">reply</p>
         @include ('comments.show', ['comments' => $comment->replies])
     </div>
 
--- resources/views/links/show.blade.php
+++ resources/views/links/show.blade.php
@@ -8,8 +8,17 @@
     @include('comments.create', ['link_id' => $link->id])
 
     @foreach ($comments as $comment)
-        <div class="comment">
-            <p>{{ $comment->body }}</p>
+        <div class="thread">
+            <div class="thread__comment comment">
+                <span class="thread__comment__author">
+                    <a href="#">{{ $comment->user->name }}</a>
+                </span>
+                <span class="thread__comment__date">
+                    {{ $comment->created_at }}
+                </span>
+                <p class="thread__comment__body">{{ $comment->body }}</p>
+                <p class="btn--reply">reply</p>
+            </div>
             @include ('comments.show', ['comments' => $comment->replies])
         </div>
     @endforeach
