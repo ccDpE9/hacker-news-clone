bc46cb42ae323f910bc54061d63d8f073f0e95d6
Kosta__ <z_eon00@protonmail.com>
2018-10-30 15:11:26 +0100

    Helper testing functions

---
+++ tests/utilities/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function make($class, $attributes = [])
+{
+    return factory($class)->make($attributes);
+}
+
+function make($class, $attributes = [])
+{
+    return factory($class)->make($attributes);
+}
