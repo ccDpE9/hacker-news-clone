23f81527febfced8a807b949591f15ac59b9095d
Kosta__ <z_eon00@protonmail.com>
2018-11-05 21:09:11 +0100

    Added two helper functions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,4 +17,13 @@ abstract class TestCase extends BaseTestCase
         return $this;
     }
 
+
+    function create($class, $attributes = []) {
+        return factory($class)->create($attributes);
+    }
+
+    function make($class, $attributes = []) {
+        return factory($class)->make($attributes);
+    }
+
 }
