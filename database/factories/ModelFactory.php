14831ee078ffb927ca4d7db16926eec459089b43
Kosta__ <z_eon00@protonmail.com>
2018-11-06 20:08:58 +0100

    Change faker method used for generating titles, so it conforms with slugs. Also, removed 'id' field, since i don't need it

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,11 +4,22 @@ use Faker\Generator as Faker;
 use App\Link;
 use App\Comment;
 
+$factory->define(App\User::class, function (Faker $faker) {
+    return [      
+        //'id' => $number++,
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+    ];
+});
+
+
 $factory->define(Link::class, function (Faker $faker) {
-    static $number = 0;
+    $title = $faker->sentence($nbWords=4, $variableNbWords=true);
     return [
-        'id' => $number++,
-        'title' => $faker->sentence,
+        'title' => $title,
+        'slug' => str_slug($title),
         'url' => $faker->url,
         'description' => $faker->text,
         'user_id' => function() {
@@ -18,22 +29,8 @@ $factory->define(Link::class, function (Faker $faker) {
 });
 
 
-$factory->define(App\User::class, function (Faker $faker) {
-    static $number = 0;
-    return [      
-        'id' => $number++,
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-    ];
-});
-
-
 $factory->define(Comment::class, function (Faker $faker) {
-    static $number = 0;
     return [
-        'id' => $number++,
         'parent_id' => Null,
         'body' => 'Comment',
         'commentable_id' => function() {
