aa89951564245c67d2450a2eef2a5dfbbe31298d
Kosta__ <z_eon00@protonmail.com>
2018-11-15 14:52:41 +0100

    createdAt getter

--- app/Comment.php
+++ app/Comment.php
@@ -12,14 +12,26 @@ class Comment extends Model
         return $this->belongsTo(User::class);
     }
 
+
     public function commentable()
     {
         return $this->morphTo();
     }
 
+
     public function replies()
     {
         return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+
+    public function getCreatedAtAttribute()
+    {
+        $now = \Carbon\Carbon::now();
+        $end = \Carbon\Carbon::parse(
+            $this->attributes['created_at']
+        );
+        return $end->diffForHumans($now);
     }
 
 }
