e2497bfb24a8b64501df7b4e0eeda5c489b5abd0
Kosta__ <z_eon00@protonmail.com>
2018-11-22 17:10:23 +0100

    Removed login route, so i had to manually define remaining auth routes, since Auth::routes() does not provide something like exlude() method

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@ Route::resource('/comments', 'CommentController')->except(['index']);
 
 
 // --- AUTH --- //
-Auth::routes();
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 // --- PROFILE --- //
