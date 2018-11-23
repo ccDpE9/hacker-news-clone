c4ebf33e053e1964f14b9ad296198501f23ca902
Kosta__ <z_eon00@protonmail.com>
2018-11-23 19:27:25 +0100

    Named register route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::resource('/comments', 'CommentController')->except(['index']);
 Route::post('login', 'Auth\LoginController@login')->name('login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::post('register', 'Auth\RegisterController@register');
+Route::post('register', 'Auth\RegisterController@register')->name('register');
 
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
