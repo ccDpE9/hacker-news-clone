fb44d7328f49b44f665be9508ed242abb9294407
Kosta__ <z_eon00@protonmail.com>
2018-11-29 15:37:03 +0100

    Things i needed to change in order to run cloned repo

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'hacker_news'),
             'username' => env('DB_USERNAME', 'comm'),
             'password' => env('DB_PASSWORD', 'comm'),
